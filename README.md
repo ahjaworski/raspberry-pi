0fc5fdd838950e0d293d857adc0f65cb81d3e6ef
Antek Jaworski <ahjaworski@gmail.com>
2020-12-30 20:36:18 +0100

    first commit with bitcoin-price-light

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# raspberry-pi
+# Raspberry-pi projecten
+## Prerequisites
+- Raspbian OS
+
+### bitcoin-price-light.py 
+1. Install GPIO zero: https://gpiozero.readthedocs.io/en/stable/installing.html
+1. Open `bitcoin-price-light.py` in `Thonny Python IDE`
+1. Run the code! (and make changes if you want different behaviour)
---
+++ bitcoin-price-light.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from time import time, sleep
+from gpiozero import LED
+
+red = LED(22)
+orange = LED(27)
+green = LED(17)
+
+red.off()
+orange.off()
+green.off()
+
+url = 'https://bitonic.nl/api/buy?btc=1'
+oldPrice = 0
+
+while True:
+    sleep(30 - time() % 30)
+    myResponse = requests.get(url)
+    if (myResponse.ok):
+        jData = json.loads(myResponse.content)
+
+        print("Bitcoin prijs eerst in euro:", oldPrice)
+        print("\n")
+        print("Bitcoin prijs nu in euro:", jData['eur'])
+        newPrice = jData['eur']
+        if (oldPrice < newPrice):
+            red.off()
+            orange.off()
+            green.blink(1, 1, 1)
+        else:
+            red.blink(1, 1, 1)
+            orange.off()
+            green.off()
+
+        if (newPrice > 24000):
+            green.on()
+
+        oldPrice = newPrice
+
+    else:
+        myResponse.raise_for_status()
